758f7b55461a39d45297b0b1c5859f55ed1af583
Mahamudol Hasan Rahat <rahat.69x1378@gmail.com>
2025-04-17 23:17:22 +0600

    Update print_the_first_N_terms_of_the_fibonacci_series.c
    added a C program to print the first N terms of the Fibonacci series using a loop with user input

--- print_the_first_N_terms_of_the_fibonacci_series.c
+++ print_the_first_N_terms_of_the_fibonacci_series.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+int main() {
+ int n, p1= 0, p2= 1, next;
+ printf("enter the number of terms: ");
+ scanf("%d",&n);
+ for
+ (int i = 1; i <= n; i++)
+ {
+ printf("%d ", p1);
+ next = p1 + p2;
+ p1 = p2;
+ p2 = next;
+ }
+ return 0;
+}
 
